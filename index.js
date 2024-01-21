804754123177a8ff1b950557f28f63d8b5a7aa64
DavidGalileo24 <david.salgado.mejia@gmail.com>
2024-01-21 15:00:53 -0600

    Working in connection: mongo database!

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
 
+mongoose.connect(process.env.DATABASE_URL, {})
+.then((res) => {
+    console.log(res)
+}) .catch((e) => {
+    console.log(e);
+});
+
+app.use(express.json());
 app.listen(port)
