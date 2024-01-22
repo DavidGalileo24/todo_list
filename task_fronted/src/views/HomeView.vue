d1a7e84010aa8188407bce4489acae9f1f729c23
DavidGalileo24 <david.salgado.mejia@gmail.com>
2024-01-22 00:11:00 -0600

    Configure global endpoint in fronted

--- task_fronted/src/views/HomeView.vue
+++ task_fronted/src/views/HomeView.vue
@@ -1,14 +1,16 @@
 <script setup>
+import { onMounted } from 'vue';
 import {useTaskStore} from '@/stores/task'
 
 const taskStore = useTaskStore();
-
+onMounted(() => {
+  console.log('gg');
+});
 
 </script>
 <template>
   <div>
     <button @click="taskStore.successAlert">Hello world</button>
-
     {{taskStore.status}}
   </div>
 </template>
