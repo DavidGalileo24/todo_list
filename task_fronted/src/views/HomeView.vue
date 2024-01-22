debfc4f3d6c8123d9ba0e094a46998fcf5f96047
DavidGalileo24 <david.salgado.mejia@gmail.com>
2024-01-21 23:45:27 -0600

    Test toast alert

--- task_fronted/src/views/HomeView.vue
+++ task_fronted/src/views/HomeView.vue
@@ -1,12 +1,19 @@
 <script setup>
-import { inject } from 'vue'
 import Swal from 'sweetalert2'
 
 const showAlert = () =>{
-  Swal.fire('Something went wrong.');
+  Swal.fire({
+    toast: true,
+    icon: 'success',
+    title: '¡Tarea guardada satisfactoriamente!',
+    position: 'bottom-right',
+    iconColor: 'white',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+  });
 }
 </script>
-
 <template>
   
   <button @click="showAlert">Hello world</button>
