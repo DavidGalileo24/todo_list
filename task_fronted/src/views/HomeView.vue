4766d4ee50a2476f5a1d7fef2393c97d6131cf83
DavidGalileo24 <david.salgado.mejia@gmail.com>
2024-01-21 23:34:51 -0600

    Added main dependencies in frontend

--- task_fronted/src/views/HomeView.vue
+++ task_fronted/src/views/HomeView.vue
@@ -1,6 +1,13 @@
 <script setup>
+import { inject } from 'vue'
+import Swal from 'sweetalert2'
+
+const showAlert = () =>{
+  Swal.fire('Something went wrong.');
+}
 </script>
 
 <template>
-  <p>Lorem</p>
+  
+  <button @click="showAlert">Hello world</button>
 </template>
