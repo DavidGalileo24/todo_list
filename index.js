00352473fe5b704cec463cb613d2e14b26839ee4
DavidGalileo24 <david.salgado.mejia@gmail.com>
2024-01-22 00:26:16 -0600

    config cors in backend project

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 const tasksRoutes = require('./src/routes/tasks');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
 mongoose.connect(process.env.DATABASE_URL, {});
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,
+    optionSuccessStatus:200
+}
 
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use('/api', tasksRoutes);
 app.listen(port)
